b49d932813e9ba950fc2235048bbf1b8ae462f4a
Evan Parker <evanparker@Evans-MacBook-Pro.local>
2022-02-14 17:21:13 -0500

    basic navbar, switching to alternate branch after this commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,20 @@
 import './App.scss';
+import { Navbar, Container, Nav } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
+
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Gotta Go!</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home"></Nav.Link>
+            <Nav.Link href="#features"></Nav.Link>
+            <Nav.Link href="#pricing"></Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
 
     </div>
   );
