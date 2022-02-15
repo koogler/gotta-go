20c4366e95f01ff4fb52c857b02f1e1ce29fb3d1
Evan Parker <evanparker@Evans-MacBook-Pro.local>
2022-02-14 21:24:02 -0500

    cleaning up directory to keep project organized

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.scss';
 import { Outlet, Link } from 'react-router-dom';
 import { Navbar, Container, Nav } from 'react-bootstrap';
-import Login from '../Login/Login';
-import useToken from './useToken';
 
 
 function App() {
-  const { token, setToken } = useToken();
-
-  if (!token) {
-    return <Login setToken={setToken} />
-  }
 
   return (
     <div>
       <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="/">Navbar</Navbar.Brand>
           <Nav className="me-auto">
             <Link to="/dashboard">Dashboard</Link>
           </Nav>
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import Login from '../Login/Login';
+import useToken from './useToken';
 
 export default function Dashboard() {
+
+  const { token, setToken } = useToken();
+
+  if (!token) {
+    return <Login setToken={setToken} />
+  }
+
   return (
     <h2>Dashboard</h2>
   );
