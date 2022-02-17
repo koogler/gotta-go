ba2f5e4deae9135d2204b76337d2d4b212d0b319
Swapnil Parab <swapnilparab@Swapnils-MacBook-Air.local>
2022-02-17 16:52:22 -0500

    styled the login form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -29,18 +29,18 @@ export default function Login({ setToken }) {
 
   return (
     <div className="login-wrapper">
-      <h1>Please Log In</h1>
-      <form onSubmit={handleSubmit}>
+      <h1 className="heading">Please Log In</h1>
+      <form className="form" onSubmit={handleSubmit}>
         <label>
           <p>Username</p>
-          <input type="text" onChange={e => setUserName(e.target.value)} />
+          <input className="form--input" type="text" onChange={e => setUserName(e.target.value)} />
         </label>
         <label>
           <p>Password</p>
-          <input type="password" onChange={e => setPassword(e.target.value)} />
+          <input className="form--input" type="password" onChange={e => setPassword(e.target.value)} />
         </label>
         <div>
-          <button type="submit">Submit</button>
+          <button className="form--submit" type="submit">Submit</button>
         </div>
       </form>
     </div>
