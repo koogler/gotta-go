c21c2606a4ca15c4c58c4b7ad664bc945ab71be9
Evan Parker <evanparker@Evans-MacBook-Pro.local>
2022-02-16 19:35:49 -0500

    added routes for locations

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const cors = require('cors');
+const morgan = require('morgan')
 const app = express();
-const pool = require("./db")
+const db = require("./db.js");
 
-app.use(cors());
+app.use(morgan("dev"))
 app.use(express.json())
 
 app.use('/login', (req, res) => {
@@ -12,6 +12,38 @@ app.use('/login', (req, res) => {
   });
 });
 
+//fetch ALL locations
+app.get('/locations', async (req, res) => {
+  const results = await db.query(`SELECT * FROM locations`)
+  console.log(results.rows)
+  res.status(200).json({
+    status: "it went through",
+    data: { locations: results.rows }
+  })
+
+})
+
+//fetch SINGLE location
+app.get('/locations/:id', async (req, res) => {
+  const results = await db.query("SELECT * FROM locations WHERE id = $1", [req.params.id])
+  res.status(200).json({
+    status: "it went through",
+    data: { locations: results.rows[0] }
+  })
+})
+
+//create location
+
+app.post('/locations', async (req, res) => {
+
+  const results = await db.query(
+    "INSERT INTO locations (name, address, latitude, longitude, open_time, close_time, accessible, changing_table, sharps_disposal, requires_purchase, privacy_rating) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11) RETURNING *",
+    [req.body.name, req.body.address, req.body.latitude, req.body.longitude, req.body.open_time, req.body.close_time, req.body.accessible, req.body.changing_table, req.body.sharps_disposal, req.body.requires_purchase, req.body.privacy_rating])
+  res.status(200).json({
+    status: "it went through",
+    data: { locations: results.rows[0] }
+  })
+})
 
 
 app.listen(8080, () => console.log('API is running on http://localhost:8080/login'));
