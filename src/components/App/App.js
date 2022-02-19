75daef15fafe01416aaafb737597e5870ec1569b
Swapnil Parab <swapnilparab@Swapnils-MacBook-Air.local>
2022-02-19 11:33:34 -0500

    1st draft for sidebar styling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -14,10 +14,18 @@ function App() {
 
   return (
 
-    <div>
-      <h1>Homepage</h1>
-      <LocationList />
-      <MapElement />
+    <div className="layout">
+      {/* <h1>Homepage</h1> */}
+      <section className="sidebar">
+        <h1 className="sidebar--centered">Menu</h1>
+        <hr className="sidebar__separator sidebar--centered" />
+        <nav className="sidebar__menu">
+          <LocationList />
+        </nav>
+      </section>
+      <section className="map">
+        <MapElement />
+      </section>
     </div >
 
   );
