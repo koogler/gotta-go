26cfaeb3688b9e7583930de84875400ce9d13147
Swapnil Parab <swapnilparab@Swapnils-MacBook-Air.local>
2022-02-22 08:11:30 -0500

    fixed some styling

--- src/components/LocationReviews/LocationReviews.js
+++ src/components/LocationReviews/LocationReviews.js
@@ -1,13 +1,15 @@
 import React from "react";
 import LocationDetail from "../LocationDetail/LocationDetail";
 
+import "./LocationReviews.scss"
+
 const LocationReviews = ({ reviews }) => {
 
   return (
     <div className="row">
       {reviews.map((review) => {
         return (
-          <div key={review.id} className="card text-white bg-primary mb-3 mr-4" style={{ maxWidth: "30%" }}>
+          <div key={review.id} className="card text-white bg-primary mb-3 mr-4" style={{ maxWidth: "40%" }}>
             <div className="card-header d-flex justify-content-between">
               <span>{review.review_body}</span>
               <span>{review.privacy_rating}</span>
