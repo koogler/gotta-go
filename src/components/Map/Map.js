6683fb3873ba8d64f6d8c05885701a7011b6cdfb
Juno-60 <woundculture@gmail.com>
2022-02-23 17:14:58 -0500

    add geolocate button, tested and working

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from "react";
+import React, { useEffect, useContext, useState } from "react";
 import LocationFind from "../../api/LocationFind";
 import { LocationsContext } from "../../context/LocationsContext";
 
@@ -13,11 +13,13 @@ const mapContainerStyle = {
   width: '100%',
   height: "92vh",
 }
+
 // sets center point (currently at union station)
 const center = {
   lat: 43.645221007945544,
   lng: -79.38060752068597,
 }
+
 // sets map "options" for customization
 const options = {
   styles: mapStyle,
@@ -27,14 +29,8 @@ const options = {
 
 let date = new Date;
 
-function GeoLocate() {
-  return <button>
-
-  </button>
-}
-
+// this is the actual map element (in case the name wasn't obvious)
 const MapElement = () => {
-
 
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
@@ -48,7 +44,7 @@ const MapElement = () => {
       try {
         const res = await LocationFind.get("/locations")
         let returnedLocations = res.data.data.locations
-        returnedLocations = returnedLocations.map((location) => {return {...location, visible: true}})
+        returnedLocations = returnedLocations.map((location) => { return { ...location, visible: true } })
         setLocations(returnedLocations)
       } catch (err) { }
     }
@@ -61,17 +57,27 @@ const MapElement = () => {
   // store a selected marker in state
   const [selected, setSelected] = React.useState(null);
 
+  // set center point, default at Union Station
+  const [center, setCenter] = useState({
+    lat: 43.645221007945544,
+    lng: -79.38060752068597,
+  });
+
   // checks for map load errors
   if (loadError) return "Error Loading Map";
   if (!isLoaded) return "Loading...";
 
   return (
+
     <div className="mapContainer">
+
       <GoogleMap
+        id="map"
         mapContainerStyle={mapContainerStyle}
         zoom={14}
         center={center}
         options={options}
+      // onLoad={onMapLoad}
       >
 
         {locations.filter((location) => location.visible).map((marker) =>
@@ -93,10 +99,10 @@ const MapElement = () => {
               <h3>{selected.name}</h3>
               <hr />
               <p>{selected.address}<br />
-              {date.toLocaleTimeString('en-US', { hour12: false }) >= selected.open_time && date.toLocaleTimeString('en-US', { hour12: false }) <= selected.close_time 
-              ? "🟢 Open Now" 
-              : "❌ Currently Closed"
-              }
+                {date.toLocaleTimeString('en-US', { hour12: false }) >= selected.open_time && date.toLocaleTimeString('en-US', { hour12: false }) <= selected.close_time
+                  ? "🟢 Open Now"
+                  : "❌ Currently Closed"
+                }
                 <hr />
                 Accessible: {selected.accessible ? "✅" : "❌"}<br />
                 Changing Table: {selected.changing_table ? "✅" : "❌"}<br />
@@ -104,6 +110,24 @@ const MapElement = () => {
                 Privacy Rating: {"⭐".repeat(selected.privacy_rating) + "★".repeat(5 - selected.privacy_rating)}</p>
             </div>
           </InfoWindow>) : null}
+
+        <button
+          className="geolocate"
+          onClick={() => {
+            navigator.geolocation.getCurrentPosition(
+              (position) => {
+                console.log(position);
+                setCenter({
+                  lat: position.coords.latitude,
+                  lng: position.coords.longitude,
+                });
+              },
+              () => null
+            );
+          }}
+        >
+          Use Current Location
+        </button>
 
       </GoogleMap>
     </div>
