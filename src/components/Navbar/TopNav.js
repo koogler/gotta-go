ba38cfeed8e6e69ee79847a179c9acaf5700bcfb
Evan Parker <evanparker@Evans-MacBook-Pro.local>
2022-02-23 12:20:20 -0500

    editing top nav bar to have app name and login/register

--- src/components/Navbar/TopNav.js
+++ src/components/Navbar/TopNav.js
@@ -9,13 +9,15 @@ function TopNav() {
     <Navbar bg="dark" variant="dark" className="header">
       <Container>
         <section className="home">
-          <Navbar.Brand href="/">Home</Navbar.Brand>
+          <Navbar.Brand href="/">Gotta Go</Navbar.Brand>
         </section>
         <Nav className="me-auto">
         </Nav>
         <Nav>
+          <Link to="/login" className="click">Login</Link>
+          <Link to="/register" className="click">Register</Link>
           <Link to="/locationadd" className="click">Add a new location!</Link>
-        </Nav>    
+        </Nav>
       </Container>
     </Navbar>
   )
