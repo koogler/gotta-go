feb82a9be862bd25e9d49970dfa52331a22eae5d
Juno-60 <woundculture@gmail.com>
2022-03-02 20:00:42 -0500

    database sequencing fix

--- server/schema/database.sql
+++ server/schema/database.sql
@@ -2,14 +2,14 @@ CREATE DATABASE gottago;
 
 DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users(
-  id SERIAL PRIMARY KEY NOT NULL, -- Sequence name: nextval('users_id_seq'::regclass)
+  id SERIAL PRIMARY KEY NOT NULL,
   username VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL
 );
 
 DROP TABLE IF EXISTS locations CASCADE;
 CREATE TABLE locations(
-  id SERIAL PRIMARY KEY NOT NULL, -- Sequence name: nextval('locations_id_seq'::regclass)
+  id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
   address VARCHAR(255) NOT NULL,
   latitude FLOAT NOT NULL,
@@ -25,7 +25,7 @@ CREATE TABLE locations(
 
 DROP TABLE IF EXISTS reviews CASCADE;
 CREATE TABLE reviews(
-  id SERIAL PRIMARY KEY NOT NULL, -- Sequence name: nextval('reviews_id_seq'::regclass)
+  id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   location_id INTEGER REFERENCES locations(id) ON DELETE CASCADE,
   review_date DATE NOT NULL DEFAULT CURRENT_DATE,
